8b2c8d0f210052792a9ad52ac4de5761123e8008
dohoanggiap2004 <dohoanggiap2004@gmail.com>
2025-05-12 22:46:21 +0700

    feat: add button thongke in home

--- app/src/main/java/haui/nhom6/qlthuvien/ui/home/HomeActivity.java
+++ app/src/main/java/haui/nhom6/qlthuvien/ui/home/HomeActivity.java
@@ -7,9 +7,10 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import haui.nhom6.qlthuvien.R;
 import haui.nhom6.qlthuvien.ui.nhanvien.NhanVienActivity;
+import haui.nhom6.qlthuvien.ui.thongke.ThongKeActivity;
 
 public class HomeActivity extends AppCompatActivity {
-    Button btnNhanVien;
+    Button btnNhanVien, btnThongKe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +18,14 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         btnNhanVien = findViewById(R.id.btnNhanVien);
+        btnThongKe = findViewById(R.id.btnThongKe);
         btnNhanVien.setOnClickListener(view -> {
             Intent intent = new Intent(HomeActivity.this, NhanVienActivity.class);
+            startActivity(intent);
+        });
+
+        btnThongKe.setOnClickListener(view -> {
+            Intent intent = new Intent(HomeActivity.this, ThongKeActivity.class);
             startActivity(intent);
         });
     }
